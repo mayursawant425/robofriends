d66a48db34581f66d63ccb15e1b9a0756cff4036
= <=>
2023-03-08 22:05:20 +0530

    Added ErrorBoundary component.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
+import ErrorBoundary from '../components/ErrorBoundry'
 import CardList from "../components/CardList";
 // import { robots } from "../robots";
 
@@ -34,7 +35,9 @@ class App extends React.Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobotList} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobotList} />
+          </ErrorBoundary>
         </Scroll>
       </div>
     );
